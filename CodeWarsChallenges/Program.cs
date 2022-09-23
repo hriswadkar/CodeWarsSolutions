b7e24d2e5edfea3b5cd266cf69de1e6336343263
hriswadkar <hriswadkar@users.noreply.github.com>
2022-09-23 21:45:11 +0530

    Adding code to main repo

---
+++ CodeWarsChallenges/Program.cs
@@ -0,0 +1,19 @@
+﻿using StripCommentsChallenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new[] { "#", "!" });
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StripCommentsChallenge/StripCommentsSolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StripCommentsChallenge
+{
+    public class StripCommentsSolution
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            System.Text.StringBuilder finalString = new System.Text.StringBuilder();
+            if (string.IsNullOrEmpty(text))
+            {
+                return "";
+            }
+
+            
+            foreach(string s in commentSymbols)
+            {
+                int index = text.IndexOf(s);
+                int newLineIndex = text.IndexOf("\n");
+                if(index > 0)
+                {
+                    finalString.Append(text.Remove(index, newLineIndex));
+                }
+            }
+
+            return finalString.ToString().TrimEnd();
+        }
+    }
+}
